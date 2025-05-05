c8cb91a729319497768b47fbd39b2111bbe7252b
maia18 <nelsonkaue05100@gmail.com>
2025-05-05 19:10:24 -0300

    42ºclass

--- Projects/Django-Agenda/contact/urls.py
+++ Projects/Django-Agenda/contact/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+
 from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('',  views.index, name='index'),
+    path('<int:contact_id>/', views.contact, name='contact'),
+    path('', views.index, name='index'),
 ]
