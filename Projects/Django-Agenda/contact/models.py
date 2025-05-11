499b8d33d955e10ebea0c9954df3a2c3a331c617
maia18 <nelsonkaue05100@gmail.com>
2025-05-11 11:45:58 -0300

    45ºclass

--- Projects/Django-Agenda/contact/models.py
+++ Projects/Django-Agenda/contact/models.py
@@ -1,14 +1,13 @@
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.db import models
+from django.utils import timezone
 
 # id (primary key - automático)
 # first_name (string), last_name (string), phone (string)
 # email (email), created_date (date), description (text)
 # category (foreign key), show (boolean), picture (imagem)
-
-# Depois
 # owner (foreign key)
+
 
 class Category(models.Model):
     class Meta:
--- Projects/Django-Agenda/contact/urls.py
+++ Projects/Django-Agenda/contact/urls.py
@@ -1,3 +1,4 @@
+# type:ignore
 from django.urls import path
 
 from contact import views
@@ -7,8 +8,9 @@ app_name = 'contact'
 urlpatterns = [
     path('', views.index, name='index'),
     path('search/', views.search, name='search'),
-    
+
     # contact (CRUD)
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/create/', views.create, name='create'),
+    path('contact/<int:contact_id>/update/', views.update, name='update'),
 ]
