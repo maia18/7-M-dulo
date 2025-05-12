1c6c8e60c194c4614b50c2a70dfcea8fbfbebf9d
maia18 <nelsonkaue05100@gmail.com>
2025-05-12 10:14:38 -0300

    46ºclass

--- Projects/Django-Agenda/contact/views/contact_forms.py
+++ Projects/Django-Agenda/contact/views/contact_forms.py
@@ -9,7 +9,7 @@ def create(request):
     form_action = reverse('contact:create')
 
     if request.method == 'POST':
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, request.FILES)
 
         context = {
             'form': form,
@@ -39,15 +39,13 @@ def create(request):
 
 
 def update(request, contact_id):
-    contact = get_object_or_404( 
-        Contact, 
-        pk=contact_id, 
-        show=True
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
     )
     form_action = reverse('contact:update', args=(contact_id,))
 
     if request.method == 'POST':
-        form = ContactForm(request.POST, instance=contact)
+        form = ContactForm(request.POST, request.FILES, instance=contact)
 
         context = {
             'form': form,
@@ -73,4 +71,24 @@ def update(request, contact_id):
         request,
         'contact/create.html',
         context
+    )
+
+
+def delete(request, contact_id):
+    contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    confirmation = request.POST.get('confirmation', 'no')
+
+    if confirmation == 'yes':
+        contact.delete()
+        return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/contact.html',
+        {
+            'contact': contact,
+            'confirmation': confirmation,
+        }
     )
